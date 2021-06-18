ab141091517646f3bfde32fa7324eb4d5a46b97c
Muhsin <askmuhsin@gmail.com>
2021-06-18 19:18:05 +0530

    dataset modified

--- utils/data.py
+++ utils/data.py
@@ -2,13 +2,13 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-class MNIST_dataset():
+class CIFAR10_dataset():
     """
-    a utility class to get data loader for MNIST.
+    a utility class to get data loader for CIFAR10.
     transforms are hardcoded for now.
     
     USAGE:
-        train_loader = MNIST_dataset(
+        train_loader = CIFAR10_dataset(
             train=True, cude=torch.cuda.is_available()
         ).get_loader()
     """
@@ -21,17 +21,12 @@ class MNIST_dataset():
         self.cuda = cuda
         self.root = root
         self.train_transforms = transforms.Compose([
-            transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,)),
-            transforms.RandomErasing(
-                p=0.5, scale=(0.02, 0.20), ratio=(0.3, 3.3), value=0
-            ),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-
         self.test_transforms = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,)),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
         if self.train:
             self.transforms = self.train_transforms
@@ -39,9 +34,14 @@ class MNIST_dataset():
             self.transforms = self.test_transforms
             
         self.shuffle = True if self.train else False
+        self.classes = (
+            'plane', 'car', 'bird', 
+            'cat', 'deer', 'dog', 'frog', 
+            'horse', 'ship', 'truck'
+        )
             
     def get_loader(self):
-        data = datasets.MNIST(
+        data = datasets.CIFAR10(
             self.root,
             train=self.train,
             transform=self.transforms,
@@ -50,7 +50,7 @@ class MNIST_dataset():
         dataloader_args = dict(
             shuffle=self.shuffle, 
             batch_size=128, 
-            num_workers=4, 
+            num_workers=2, 
             pin_memory=True
         ) if self.cuda else dict(
             shuffle=self.shuffle, 
