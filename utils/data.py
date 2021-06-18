fb6a8db3a2f13e869c5e819285e163b3bc325d4a
Muhsin <askmuhsin@gmail.com>
2021-06-18 22:20:43 +0530

    new funcs to dict

--- utils/data.py
+++ utils/data.py
@@ -16,17 +16,27 @@ class CIFAR10_dataset():
         self,
         train, cuda,
         root='./data',
+        normalized=True
     ):
         self.train = train
         self.cuda = cuda
         self.root = root
-        self.train_transforms = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+        
+        self.mean = (0.491, 0.482, 0.447)
+        self.std = (0.247, 0.243, 0.262)
+        
+        if normalized:
+            self.train_transforms = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(self.mean, self.std)
+            ])
+        else:
+            self.train_transforms = transforms.Compose([
+                transforms.ToTensor()
+            ])
         self.test_transforms = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize(self.mean, self.std)
         ])
         if self.train:
             self.transforms = self.train_transforms
@@ -34,19 +44,22 @@ class CIFAR10_dataset():
             self.transforms = self.test_transforms
             
         self.shuffle = True if self.train else False
-        self.classes = (
-            'plane', 'car', 'bird', 
-            'cat', 'deer', 'dog', 'frog', 
-            'horse', 'ship', 'truck'
-        )
-            
-    def get_loader(self):
+        self.classes = None
+        
+        
+    def get_data(self):
         data = datasets.CIFAR10(
             self.root,
             train=self.train,
             transform=self.transforms,
             download=True
         )
+        self.classes = data.classes
+        return data
+            
+    def get_loader(self):
+        data = self.get_data()
+        
         dataloader_args = dict(
             shuffle=self.shuffle, 
             batch_size=128, 
--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,5 @@
 import torch
+import random
 import torchvision
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,12 +28,36 @@ def set_seed(seed=1):
     print(f"[INFO] seed set {seed}")
 
 
-def show_random_images(data_loader):
-    data, target  = next(iter(data_loader))
-    grid_img = torchvision.utils.make_grid(data.cpu().detach())
+def show_random_images_for_each_class(
+    train_data,
+    num_images_per_class=16
+):
+    for c, cls in enumerate(train_data.classes):
+        rand_targets = random.sample([
+            n
+            for n, x in enumerate(train_data.targets)
+            if x==c
+        ], k=num_images_per_class)
+        show_img_grid(
+            np.transpose(train_data.data[rand_targets], axes=(0, 3, 1, 2))
+        )
+        plt.title(cls)
+    
+
+def show_img_grid(data):
+    try:
+        grid_img = torchvision.utils.make_grid(data.cpu().detach())
+    except:
+        data = torch.from_numpy(data)
+        grid_img = torchvision.utils.make_grid(data)
     
     plt.figure(figsize=(10, 10))
     plt.imshow(grid_img.permute(1, 2, 0))
+    
+
+def show_random_images(data_loader):
+    data, target  = next(iter(data_loader))
+    show_img_grid(data)
 
 
 def show_model_summary(model, input_size=(1, 28, 28)):
